4dd30d2b26e8588236362ca2f1fa6c71ef32bb29
Smerlyn García <Smerlyn_Alejandro@outlook.com>
2025-12-01 18:57:08 -0400

    Agregando Repositorio

--- DiarioEntretenimiento.AccesoDatos/Modelos/ItemEntretenimiento.cs
+++ DiarioEntretenimiento.AccesoDatos/Modelos/ItemEntretenimiento.cs
@@ -7,7 +7,7 @@
         public string Tipo { get; set; }
         public string Estado { get; set; }
         public byte Calificacion { get; set; }
-        public string ImagenURL { get; set; }
-        public string Comentario { get; set; }
+        public string ImagenURL { get; set; } = string.Empty;
+        public string Comentario { get; set; } = string.Empty;
     }
 }
---
+++ DiarioEntretenimiento.AccesoDatos/Repositorios/RepositorioEntretenimiento.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using DiarioEntretenimiento.AccesoDatos.Modelos;
+
+namespace DiarioEntretenimiento.AccesoDatos.Repositorios
+{
+    public class RepositorioEntretenimiento
+    {
+        private readonly string _cadenaConexion;
+
+        public RepositorioEntretenimiento(string cadenaConexion)
+        {
+            _cadenaConexion = cadenaConexion;
+        }
+
+        public List<ItemEntretenimiento> ObtenerTodo()
+        {
+            var lista = new List<ItemEntretenimiento>();
+
+            using (var conexion = new SqlConnection(_cadenaConexion))
+            {
+                using (var comando = new SqlCommand("PA_ObtenerEntretenimiento", conexion))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    conexion.Open();
+
+                    using (var lector = comando.ExecuteReader())
+                    {
+                        while (lector.Read())
+                        {
+                            lista.Add(new ItemEntretenimiento
+                            {
+                                Id = Convert.ToInt32(lector["Id"]),
+                                Titulo = lector["Titulo"].ToString(),
+                                Tipo = lector["Tipo"].ToString(),
+                                Estado = lector["Estado"].ToString(),
+                                Calificacion = Convert.ToByte(lector["Calificacion"]),
+                                ImagenURL = lector["ImagenURL"] != DBNull.Value ? lector["ImagenURL"].ToString() : null,
+                                Comentario = lector["Comentario"] != DBNull.Value ? lector["Calificacion"].ToString() : null
+                            });
+                        }
+                    }
+                }
+
+                return lista;
+            }
+        }
+    }
+}
