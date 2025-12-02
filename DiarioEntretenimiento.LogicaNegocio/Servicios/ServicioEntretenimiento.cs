b50d2b32a954df9635fda0b1ef3eb908d65f1735
Smerlyn García <Smerlyn_Alejandro@outlook.com>
2025-12-02 18:35:21 -0400

    Agregando la lógica de negocios

---
+++ DiarioEntretenimiento.LogicaNegocio/Servicios/ServicioEntretenimiento.cs
@@ -0,0 +1,20 @@
+﻿using DiarioEntretenimiento.AccesoDatos.Modelos;
+using DiarioEntretenimiento.AccesoDatos.Repositorios;
+
+namespace DiarioEntretenimiento.LogicaNegocio.Servicios
+{
+    public class ServicioEntretenimiento
+    {
+        private readonly RepositorioEntretenimiento _repositorio;
+
+        public ServicioEntretenimiento(string cadenaConexion)
+        {
+            _repositorio = new RepositorioEntretenimiento(cadenaConexion);
+        }
+
+        public List<ItemEntretenimiento> ObtenerCatalogo()
+        {
+            return _repositorio.ObtenerTodo();
+        }
+    }
+}
