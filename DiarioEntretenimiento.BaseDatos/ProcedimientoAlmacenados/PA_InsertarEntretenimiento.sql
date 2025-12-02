98f2a272f708ce4f3f19b26459daecce2b73e645
Smerlyn García <Smerlyn_Alejandro@outlook.com>
2025-12-02 19:15:38 -0400

    Agregando procedimientos de insersión y a proceder a modificar el program para hacer pruebas desde Postman

---
+++ DiarioEntretenimiento.BaseDatos/ProcedimientoAlmacenados/PA_InsertarEntretenimiento.sql
@@ -0,0 +1,21 @@
+﻿USE DiarioEntretenimientoDB;
+GO
+
+CREATE OR ALTER PROCEDURE PA_InsertarEntretenimiento
+    @Titulo NVARCHAR(100),
+    @Tipo NVARCHAR(20),
+    @Estado NVARCHAR(20),
+    @Calificacion TINYINT,
+    @ImagenUrl NVARCHAR(500),
+    @Comentario NVARCHAR(MAX)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    INSERT INTO Entretenimiento (Titulo, Tipo, Estado, Calificacion, ImagenUrl, Comentario)
+    VALUES (@Titulo, @Tipo, @Estado, @Calificacion, @ImagenUrl, @Comentario);
+
+    -- Devuelve el ID que se acaba de generar (clave para el backend)
+    SELECT CAST(SCOPE_IDENTITY() AS INT) AS NuevoId;
+END;
+GO
---
+++ DiarioEntretenimiento.BaseDatos/TestScripts/InsertItemDiarioEntretenimiento.sql
@@ -0,0 +1,16 @@
+﻿USE DiarioEntretenimientoDB;
+GO
+
+-- Ejecutamos el SP con los datos del libro
+EXEC PA_InsertarEntretenimiento 
+    @Titulo = 'Apología de Sócrates',
+    @Tipo = 'Libro',
+    @Estado = 'Finalizado',
+    @Calificacion = 10, -- ¡Un clásico merece un 10!
+    @ImagenUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/David_-_The_Death_of_Socrates.jpg/800px-David_-_The_Death_of_Socrates.jpg',
+    @Comentario = 'Obra de Platón que detalla la defensa de Sócrates durante su juicio. Una lectura fundamental sobre ética y filosofía.';
+GO
+
+-- Verificación rápida para que veas que quedó guardado
+SELECT * FROM Entretenimiento WHERE Titulo = 'Apología de Sócrates';
+GO
